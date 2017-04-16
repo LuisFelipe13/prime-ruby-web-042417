ab4c793d5088d627cec8908204d500a8d3788036
Luis del Cueto <luisdelcueto@live.com>
2017-04-16 02:13:18 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(num)
+  answer = true
+  if num <= 0 || num == 1
+    return false
+  end
+  checker = (2..num-1).to_a
+  checker = checker.reverse
+  checker.each do |number|
+    if num % number == 0
+      answer = false
+    end
+  end
+  answer
+end
